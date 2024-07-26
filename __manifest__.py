99bd32c5f91a25a91a3f3d4aeef1c63a432978ff
angel <angelmendez00512@gmail.com>
2024-07-26 08:50:31 -0600

    fase 1 reporte

--- __manifest__.py
+++ __manifest__.py
@@ -12,8 +12,11 @@
     'website': '',
     'depends': ['sale', 'mrp'],
     'data': [
+        'report/sale_order_report_template.xml',
+        'report/sale_order_report.xml',
         'security/ir.model.access.csv',
         'views/sale_order_views.xml',
+        
     ],
     'installable': True,
     'application': False,
