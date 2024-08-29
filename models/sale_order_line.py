3227a0d54bedc4087dcdc779d4da50f823c14952
angel <angelmendez00512@gmail.com>
2024-08-29 12:17:45 -0600

    Fix cotizador mostar cotizador

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -4,8 +4,8 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     type_id = fields.Many2one('product.type', string="Tipo")
-    show_quotation = fields.Boolean(string="Show Quotation")
-    show_price = fields.Boolean(string="Show Price")
+    show_quotation = fields.Boolean(string="Mostrar Cotización")
+    show_price = fields.Boolean(string="Mostrar Precio")
 
     @api.onchange('type_id')
     def _onchange_type_id(self):
